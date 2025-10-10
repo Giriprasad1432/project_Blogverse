08e4e72b98d13c20e406706afac891e1552aba66
Giriprasad1432 <giribabu121989@gmail.com>
2025-10-10 14:38:13 +0530

    404 eror fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,18 @@ import NavBar from "./NavBar"
 import SignUp from "./SignUp"
 import SignIn from "./SignIn"
 import Home from "./Home"
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  
+
 
 
   return (
     <Routes>
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/login" element={<SignIn/>}/>
-      <Route path="/" element={<Home/>}/>
-      </Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
